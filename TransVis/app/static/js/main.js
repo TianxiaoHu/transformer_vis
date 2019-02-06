b1ea81ff0cff3c7d9be5458a0cf8469597a03ea5
tianxiaohu <hutianxiao_fdu@126.com>
2019-02-06 19:54:40 +0800

    update heatmap

--- TransVis/app/static/js/main.js
+++ TransVis/app/static/js/main.js
@@ -1,31 +1,19 @@
 $(document).ready(function() {
     $('.ui.dropdown')
         .dropdown();
-
-    // TESTER = document.getElementById('highlight');
-    //     // Plotly.plot(TESTER, [{
-    //     //     x: [1, 2, 3, 4, 5],
-    //     //     y: [1, 2, 4, 8, 16]
-    //     // }], {
-    //     //     margin: { t: 0 }
-    //     // });
+    d3.json("../static/data/heatmap.json", function(error, data) {
+        heatmapdata = data;
 
     function drawImage(imageObj) {
         var canvas = document.getElementById("heatmapCanvas");
         var context = canvas.getContext("2d");
         var imageX = 0;
         var imageY = 0;
-        var imageWidth = imageObj.width;
-        var imageHeight = imageObj.height;
 
         context.drawImage(imageObj, imageX, imageY);
 
-        // var imageData = context.getImageData(imageX, imageY, imageWidth, imageHeight);
-        // var data = imageData.data;
-
-        var zdata=[[1, 2, 3], [2, 3, 4], [3, 4, 5]];
         var data = [{
-            z: zdata,
+            z: heatmapdata,
             type: "heatmapgl",
             colorscale: "Picnic"
         }];
@@ -38,4 +26,6 @@ $(document).ready(function() {
     };
     imageObj.crossOrigin = "Anonymous";
     imageObj.src = "https://images.plot.ly/plotly-documentation/images/heatmap-galaxy.jpg";
+    });
+
 });
